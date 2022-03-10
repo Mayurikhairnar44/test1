76a3ba4ea5b4c1c73b3ffe8035590b6f84fc9928
Mayurikhairnar44 <92872200+Mayurikhairnar44@users.noreply.github.com>
2022-03-10 16:13:41 +0530

    Update LP2.java

--- LP2.java
+++ LP2.java
@@ -1,7 +1,6 @@
-class main 
+public class Main
 {
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello, World!"); 
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
